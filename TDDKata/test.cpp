ca9e192d05a9c18f35d77592070869bba4039e96
Станислав Конотоп <s.konotop@owen.ua>
2020-12-02 09:50:12 +0200

    рефакторинг тестов

--- TDDKata/test.cpp
+++ TDDKata/test.cpp
@@ -1,191 +1,254 @@
 #include "pch.h"
 #include "Calculator.h"
+#include "TestFixture.h"
 
-	TEST(TDDKata, NoStr) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add(""), 0);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, NoStr) {
+		char* op = ""; 
+		int expected = 0;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestName1) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1"), 1);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestName1) {
+		char* op = "1";
+		int expected = 1;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestName19) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("123456789"), 123456789);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestName19) {
+		char* op = "123456789";
+		int expected = 123456789;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestName2) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1"), 2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestName2) {
+		char* op = "1,1";
+		int expected = 2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestName3333) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1111,2222"), 3333);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestName3333) {
+		char* op = "1111,2222";
+		int expected = 3333;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
 
-	TEST(TDDKata, TestName3) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1,1"), 3);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestName3) {
+		char* op = "1,1,1";
+		int expected = 3;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Zero) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("0,1"), 1);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Zero) {
+		char* op = "0,1";
+		int expected = 1;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Zero2) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("00,1"), 1);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Zero2) {
+		char* op = "00,1";
+		int expected = 1;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Zero1) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("00,000"), 0);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Zero1) {
+		char* op = "00,000";
+		int expected = 0;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Zero3) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("01,002"), 3);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Zero3) {
+		char* op = "01,002";
+		int expected = 3;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Neg) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("-1,2"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Neg) {
+		char* op = "-1,2";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 	
-	TEST(TDDKata, Dot) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1.1"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Dot) {
+		char* op = "1.1";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 	
-	TEST(TDDKata, Dot2) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1.1"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Dot2) {
+		char* op = "1,1.1";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 	
-	TEST(TDDKata, Format) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add(",1,1"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Format) {
+		char* op = ",1,1";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Format2) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1,"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Format2) {
+		char* op = "1,1,";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Format3) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("a,z"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Format3) {
+		char* op = "a,z";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 	
-	TEST(TDDKata, Param5) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1,1,1,1"), 5);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, Param5) {
+		char* op = "1,1,1,1,1";
+		int expected = 5;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, maxParam) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1000001"), 1000015);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, maxParam) {
+		char* op = "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1000001";
+		int expected = 1000015;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, NullTest) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add(NULL), -3);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, NullTest) {
+		char* op = NULL;
+		int expected = -3;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
 
-	TEST(TDDKata, TestDelimiter) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1\n1,1,1,1,1\n1,1,1,1,1,1\n1,1"), 15);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestDelimiter) {
+		char* op = "1,1\n1,1,1,1,1\n1,1,1,1,1,1\n1,1";
+		int expected = 15;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 	
-	TEST(TDDKata, TestDelimiter1) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1,\n1,1,1,1,1,1,1,1,1,1,1,1,1"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestDelimiter1) {
+		char* op = "1,1,\n1,1,1,1,1,1,1,1,1,1,1,1,1";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestDelimiter2) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1\n,1,1,1,1,1,1,1,1,1,1,1,1,1"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestDelimiter2) {
+		char* op = "1,1\n,1,1,1,1,1,1,1,1,1,1,1,1,1";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestDelimiter3) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1,1\n\n1,1,1,1,1,1,1,1,1,1,1,1,1"), -2);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestDelimiter3) {
+		char* op = "1,1\n\n1,1,1,1,1,1,1,1,1,1,1,1,1";
+		int expected = -2;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 	
-	TEST(TDDKata, TestNewDelimiter) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("//;\n1;1;1;1;1;1;1;1;1;1;1;1;1;1;1"), 15);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestNewDelimiter) {
+		char* op = "//;\n1;1;1;1;1;1;1;1;1;1;1;1;1;1;1";
+		int expected = 15;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestNewDelimiter1) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("//\n;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1"), ERR_CHANGE_DELIMITER_FORMAT);
-		ASSERT_TRUE(true);
-	}
-	TEST(TDDKata, TestNewDelimiter2) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("//\n\n;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1"), ERR_CHANGE_DELIMITER_FORMAT);
-		ASSERT_TRUE(true);
-	}
+	TEST_F(TestFixture, TestNewDelimiter1) {
+		char* op = "//\n;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1";
+		int expected = ERR_CHANGE_DELIMITER_FORMAT;
 
-	TEST(TDDKata, TestNewDelimiter3) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("//z\nz1z1z1"), ERR_FORMAT);
-		ASSERT_TRUE(true);
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
+	}
+	TEST_F(TestFixture, TestNewDelimiter2) {
+		char* op = "//\n\n;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1";
+		int expected = ERR_CHANGE_DELIMITER_FORMAT;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestNewDelimiter5) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("//z\n1z1z1"), 3);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestNewDelimiter3) {
+		char* op = "//z\nz1z1z1";
+		int expected = ERR_FORMAT;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestNewDelimiter6) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("//z\n1z1,1"), ERR_FORMAT);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestNewDelimiter5) {
+		char* op = "//z\n1z1z1";
+		int expected = 3;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, TestNewDelimiter4) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("//1\n1211223121"), ERR_CHANGE_DELIMITER_FORMAT);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestNewDelimiter6) {
+		char* op = "//z\n1z1,1";
+		int expected = ERR_FORMAT;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
+	}
+
+	TEST_F(TestFixture, TestNewDelimiter4) {
+		char* op = "//1\n1211223121";
+		int expected = ERR_CHANGE_DELIMITER_FORMAT;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
 
-	TEST(TDDKata, TestNewDelimiter7) {
-		Calculator calc;
-		ASSERT_EQ(calc.Add("1//.\n,1,1,1,1,1,1,1,1,1,1,1,1,1,1"), ERR_FORMAT);
-		ASSERT_TRUE(true);
+	TEST_F(TestFixture, TestNewDelimiter7) {
+		char* op = "1//.\n,1,1,1,1,1,1,1,1,1,1,1,1,1,1";
+		int expected = ERR_FORMAT;
+
+		int actual = calc->Add(op);
+		ASSERT_EQ(expected, actual);
 	}
 
